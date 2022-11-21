0c813827bd160b8554e900c13191fe7922e3e5c7
FranAM1 <falmansamartinez@cifpfbmoll.eu>
2022-11-21 13:51:32 +0100

    home + header + footer responsive

--- src/js/header.js
+++ src/js/header.js
@@ -1,7 +1,31 @@
-const header = document.getElementById("header")
+const header = document.getElementById("header");
 
 fetch("./components/header.html")
-    .then(res=>res.text())
-    .then(data => {
-        header.innerHTML += data
-    })
+  .then((res) => res.text())
+  .then((data) => {
+    header.innerHTML += data;
+  });
+
+window.onload = () =>{
+    if (window.innerWidth < 876) {
+        document.getElementById("iconoUsuario").className = "fas fa-user fa-2x";
+    }else{
+        document.getElementById("iconoUsuario").className = "fas fa-user fa-5x";
+    }
+}
+
+window.addEventListener("resize", () => {
+    if (window.innerWidth < 876) {
+        document.getElementById("iconoUsuario").className = "fas fa-user fa-2x";
+    }else{
+        document.getElementById("iconoUsuario").className = "fas fa-user fa-5x";
+    }
+})
+
+function abrirNav(){
+    document.getElementsByTagName("nav")[0].className = "mostrarNav";
+}
+
+function cerrarNav(){
+    document.getElementsByTagName("nav")[0].className = "ocultarNav";
+}
