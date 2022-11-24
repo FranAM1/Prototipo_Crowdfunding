553f38deada352ff415c613849b5436ea3961989
FranAM1 <falmansamartinez@cifpfbmoll.eu>
2022-11-24 16:21:31 +0100

    barra buscador mobile

--- src/js/header.js
+++ src/js/header.js
@@ -12,6 +12,26 @@ function abrirNav(){
 }
 
 function cerrarNav(){
-    document.getElementsByTagName("nav")[0].className = "ocultarNav";
-    document.getElementById("navFondo").className = "ocultarNav"
+    document.getElementsByTagName("nav")[0].className = "ocultar";
+    document.getElementById("navFondo").className = "ocultar"
 }
+
+function mostrarBuscador(){
+  if(window.innerWidth <= 875){
+    document.getElementById("menuBuscadorMobile").className = "buscadorMobile"
+  }
+}
+
+function ocultarBuscador(){
+  document.getElementById("menuBuscadorMobile").className = "ocultar"
+}
+
+window.addEventListener("resize", ()=>{
+  if(window.innerWidth > 875 && document.getElementsByTagName("nav")[0].className == "ocultar"){   
+    document.getElementsByTagName("nav")[0].className = "";
+  }
+  
+  if(window.innerWidth > 875 && document.getElementById("menuBuscadorMobile").className == "buscadorMobile"){
+    ocultarBuscador()
+  }
+})
